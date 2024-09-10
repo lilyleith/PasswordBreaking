ac2454ccce99a3c3455313e23108abf75279db20
Lily Leith <129114315+lilyleith@users.noreply.github.com>
2024-09-10 20:21:23 +0000

    changes to password_cracker to introduce hashing the shadow file

--- password_cracker.py
+++ password_cracker.py
@@ -7,19 +7,33 @@ import time
 - 
  """
 start_time = time.perf_counter()
-d = open("dictionary.txt", "r")
+dictionary = open("dictionary.txt", "r")
+
 outFiles = ["myout1.txt", "myout2.txt", "myout3.txt"]
 
 hashes = ["sha1", "md5", "sha256"]
+passwords = {}
+hashedPasswords = {}
+with open("shadow", "r") as f2:
+    for userPassword in f2:
+        userPass = userPassword.split(":")
+        passwords[userPass[0]] = userPass[1]
+        hashedPasswords[userPass[0]] = ""
+    
 
 
-for h in hashes:
-    hash = hashlib.new(h)
-    for fOut in outFiles:
-        with open(fOut,"w") as out:
-            for word in d:
-                hash.update(word.encode())
-                out.write(hash.hexdigest() + "\n")
+for user in passwords.keys():
+    pws = passwords[user]
+    for h in hashes:
+        hash = hashlib.new(h)
+        hash.update(pws)
+        hashedPasswords[user] = hash.hexdigest()
+        """ for fOut in outFiles:
+            with open(fOut,"w") as out:
+                for word in d:
+                    hash2 = hashlib.new(h)
+                    hash2.update(word.encode())
+                    out.write(hash2.hexdigest() + "\n") """
 
 end_time = time.perf_counter()
 elapsed_time = end_time - start_time
