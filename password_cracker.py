a4b99973b4330b6a933837f355d6f0d55b1a3096
Lily Leith <129114315+lilyleith@users.noreply.github.com>
2024-09-13 21:18:55 +0000

    changes to password cracker to translate to leetspeak

--- password_cracker.py
+++ password_cracker.py
@@ -52,23 +52,54 @@ def sha1(plaintext):
     hash.update(plaintext.encode())
     return hash.hexdigest()
 
+def sha224(plaintext):
+    hash = hashlib.new("sha224")
+    hash.update(plaintext.encode())
+    return hash.hexdigest()
+
 def sha256(plaintext):
     hash = hashlib.new("sha256")
     hash.update(plaintext.encode())
     return hash.hexdigest()
 
 # translate a word into leetspeak
-def leetSpeakConversion(word):
-    word = word.replace("a", "4")
-    word = word.replace("b", "8") # this is a maybe
-    word = word.replace("e", "3")
-    word = word.replace("g", "6") # maybe
-    word = word.replace("i", "1")
-    word = word.replace("l", "1")
-    word = word.replace("o", "0")
-    word = word.replace("s", "5")
-    word = word.replace("t", "7")
-    word = word.replace("z", "2")
+def leetSpeakConversion(word, iteration):
+# 2 iterations as of right now
+    translations = {"a": ["4","@"], "b":["8", "8"], "c":["c", "["], "d":["d", ")"], "e":["3", "&"], "f":["f"], "g":["6", "&"], "h":["h", '#'], "i":["i", "!"], "j":["j", "j"], "k":["k", "k"], "l":["1","!"], "m":["m", "m"],"n":["n", "n"], "o":["0", "0"], "p":["p", "p"], "q":["q", "q"], "r":["r", "r"], "s":["5", "$"], "t":["7", "+"], "u":["u", "u"], "v":["v", "v"], "w":["w", "w"], "x":["x", "x"], "y":["y", "y"], "z":["2", "2"], "q":["9"]}
+   
+# MAX INDEX = 2 LEN 3 INDEX - INDEX LEN MINUS LEN
+    leetWord = "".join([translations[letter][0] for letter in word])
+
+    for i in range(2):
+
+
+
+
+    
+
+    if forward == 1:
+        word = word.replace("a", "4")
+        word = word.replace("b", "8") # this is a maybe
+        word = word.replace("e", "3")
+        word = word.replace("g", "6") # maybe
+        word = word.replace("l", "1")
+       # word = word.replace("i", "!")
+        word = word.replace("o", "0")
+        word = word.replace("s", "5")
+        word = word.replace("t", "7")
+        word = word.replace("z", "2")
+        word = word.replace("q", "9")
+    else:
+        word = word.replace("4", "a")
+        word = word.replace("8", "b") # this is a maybe
+        word = word.replace("3", "e")
+        word = word.replace("6", "g") # maybe
+        word = word.replace("1", "l")
+       # word = word.replace("!", "i")
+        word = word.replace("0", "o")
+        word = word.replace("5", "s")
+        word = word.replace("7", "t")
+        word = word.replace("2", "z")
     return word
 
 # this function checks the simplest case in which the password is only encrypted with one hash function
@@ -79,10 +110,10 @@ def checkSimpleHashes(password):
         for word in dictionary:
             # take out newline character from dictionary word
             word = word.replace("\n", "")
-
+            
             # get the leet speak conversion
-            leetSpeakWord = leetSpeakConversion(word)
-            print(leetSpeakWord)
+            leetSpeakWord = leetSpeakConversion(word, 1)
+            #print(leetSpeakWord)
             # the length of md5 output is 32, so we can specify to check md5 for passwords of length 32
             # check the md5 hash for both the leet speak version and the regular word
             if len(password) == 32:
@@ -93,7 +124,7 @@ def checkSimpleHashes(password):
                     return word
                 
                 if hashedLeetSpeak == password:
-                    return word
+                    return leetSpeakWord
             # the length of sha1 output is 40, so we can specify to check sha1 for passwords of length 40
             # check if sha1 gives result, if it does then set the key-value pair in truePasswords  
             if len(password) == 40:
@@ -103,8 +134,18 @@ def checkSimpleHashes(password):
                     return word
             
                 if hashedLeetSpeak == password:
+                    return leetSpeakWord
+                
+            # the length of sha224 output is 28, so we can specify to check sha224 for passwords of length 28
+            # check if sha224 gives result, if it does then set the key-value pair in truePasswords
+            if len(password) == 28:
+                hashedWord = sha224(word)
+                hashedLeetSpeak = sha224(leetSpeakWord)
+                if hashedWord == password:
                     return word
-
+                if hashedLeetSpeak == password:
+                    return leetSpeakWord
+                    
             # the length of sha256 output is 64, so we can specify to check sha256 for passwords of length 64
             # check if sha256 gives result, if it does then set the key-value pair in truePasswords
             if len(password) == 64:
@@ -113,7 +154,7 @@ def checkSimpleHashes(password):
                 if hashedWord == password:
                     return word
                 if hashedLeetSpeak == password:
-                    return word
+                    return leetSpeakWord
     return ""
 
 
@@ -148,7 +189,15 @@ if __name__ == '__main__':
         
         # add each user and password combination to the hashedPasswords dict
         hashedPasswords[user] = password
-
+        leetSpeakWords = {}
+        dictionary = open("dictionary.txt","r")
+        for word in dictionary:
+            leetSpeakWords[word] = []
+            for i in range(2):
+                leetSpeakWord = leetSpeakConversion(word, i)
+                leetSpeakWords[word][i] = leetSpeakWord
+        
+        
         # add result of the checkSimpleHashes() function to the truePasswords dict if it's a valid
         # password
         simpleResult = checkSimpleHashes(password)
